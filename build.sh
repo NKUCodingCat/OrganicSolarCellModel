4d10944841a35c575329ce7be315e87d5c91c43c
JingTao Chen <chenjingtao930@gmail.com>
2018-12-31 21:19:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ cd bin
 gfortran -O3 ../g09log.f90 -o g09log.exe -static
 gfortran -O3 ../calv/transferintegral.f90 -o calv -lopenblas -llapack -static
 gfortran -O3 ../gfilm/split.f90 ../gfilm/fileio.f90 -o split -static
+gfortran -O3 ../rw/rw_serial.f90 -o rw -static
